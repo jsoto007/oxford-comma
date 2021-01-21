9812144df411eb4f57fb903f8ad9e1456a337015
Jose Soto <jsoto7087@gmail.com>
2021-01-21 16:19:14 -0500

    commit tree 2c1b5acea06757fc761041684d3d6b5572bb2486

--- Gemfile
+++ Gemfile
@@ -1,4 +1,13 @@
 # A sample Gemfile
 source "https://rubygems.org"
+# require "pry"
+# def oxford_comma(array)
+#     binding.pry
+# #new_array = []
+# #array.split(", ") do |arr|
+#  #   new_array << (arr.split)
+# # end
+# # return new_array
+#  end
 
-# gem "rails"
+# # gem "rails"
--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,31 @@
-def oxford_comma(array)
 
-end
+   
+    def oxford_comma(array)
+#         require "pry"   
+   if array.length == 1           
+    array.join
+     elsif array.length == 2
+         array.join (" and ")
+         #binding.pry
+    else 
+         last = array.pop 
+#         #array.join (", ")
+#         #array << ", and #{last}"
+         array << ("and #{last}")
+        array.join (", ") #last becasue the program is specting a string not an array
+        
+
+     end
+
+ end
+
+
+    #new_array = []
+    #array.split(", ") do |arr|
+     #   new_array << (arr.split)
+    # end
+    # return new_array
+
+
+    
+    # gem "rails"
